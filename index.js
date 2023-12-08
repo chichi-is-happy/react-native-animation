00a19f3b6624a9ddfd57ba6a3ee0ae51eca1bdc0
치치 <seonrye.yun@gmail.com>
2023-12-08 11:40:38 +0900

    feat: 두번째 게임 구현 중
    * 이미지 드래그까지 구현
    * react-native-gesture-handler 추가
    * TODO: 버튼에 이미지를 올리면 실행되는 동작들 구현 필요 - 버튼 크기 증가, 색상 변경, 해당 이미지 사라짐 등

--- index.js
+++ index.js
@@ -5,9 +5,13 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import First from './src/pages/firstGame/First';
+import Second from './src/pages/secondGame/Second';
 import { name as appName } from './app.json';
 
 // AppRegistry.registerComponent(appName, () => First);
-AppRegistry.registerComponent(appName, () => First, {
+// AppRegistry.registerComponent(appName, () => First, {
+//   bundleURL: 'http://localhost:8081/index.bundle?platform=ios&dev=true',
+// });
+AppRegistry.registerComponent(appName, () => Second, {
   bundleURL: 'http://localhost:8081/index.bundle?platform=ios&dev=true',
 });
--- src/pages/secondGame/Second.js
+++ src/pages/secondGame/Second.js
@@ -1,96 +1,145 @@
 import React, { useState } from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  Button,
-  useColorScheme,
-  View,
-  TouchableOpacity,
-  Image,
-} from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
 import FastImage from 'react-native-fast-image';
-import image1 from '../../images/1.jpeg';
-import image2 from '../../images/2.jpeg';
-import image3 from '../../images/3.jpeg';
-import image4 from '../../images/4.jpeg';
-import image5 from '../../images/5.jpeg';
-import Animated from 'react-native-reanimated';
+import Animated, {
+  useSharedValue,
+  withSpring,
+  useAnimatedStyle,
+  withTiming,
+} from 'react-native-reanimated';
+import { Circle } from 'react-native-svg';
+import { Gesture, GestureDetector, GestureHandlerRootView } from 'react-native-gesture-handler';
+const AnimatedCircle = Animated.createAnimatedComponent(Circle);
+import imageList from '../../const/imageList';
 
 const Second = () => {
-  const imageList = [image1, image2, image3, image4, image5];
+  const [prevImage, setPrevImage] = useState(imageList[0]);
   const [image, setImage] = useState(imageList[0]);
   const [oList, setOList] = useState([]);
   const [xList, setXList] = useState([]);
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(1);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+  const rotate = useSharedValue(0);
+  const pressed = useSharedValue(false);
+  const offsetX = useSharedValue(0);
+  const offsetY = useSharedValue(0);
+
+  const pan = Gesture.Pan()
+    .onBegin(() => {
+      pressed.value = true;
+    })
+    .onChange(event => {
+      offsetX.value = event.translationX;
+      offsetY.value = event.translationY;
+    })
+    .onFinalize(() => {
+      offsetX.value = withSpring(0);
+      offsetY.value = withSpring(0);
+      pressed.value = false;
+    });
+
+  const tap = Gesture.Tap()
+    .onBegin(() => {
+      pressed.value = true;
+      console.log('true !!!');
+    })
+    .onFinalize(() => {
+      pressed.value = false;
+    });
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    backgroundColor: pressed.value ? '#FFE04B' : '#B58DF1',
+    transform: [
+      { translateX: offsetX.value },
+      { translateY: offsetY.value },
+      { scale: withTiming(pressed.value ? 1.2 : 1) },
+    ],
+  }));
+
+  const animatedDefault = useAnimatedStyle(() => ({
+    transform: [
+      { translateX: translateX.value },
+      { translateY: translateY.value },
+      { rotate: `${rotate.value}deg` },
+    ],
+    zIndex: -1,
+  }));
+
   const handleClickO = targetImage => {
+    translateX.value = 0;
     setOList(oList => [...oList, targetImage]);
+    setPrevImage(image);
     setCount(count => count + 1);
     setImage(imageList[count]);
+    translateX.value = withTiming(-500, { duration: 300 });
+    rotate.value = withTiming(-360, { duration: 500 });
+
     console.log('count 확인 ::::', count);
     console.log('oList 확인 ::::', oList);
     console.log('imageList 확인 ::::', imageList);
+    console.log('image 확인:::', image);
   };
 
   const handleClickX = targetImage => {
+    translateX.value = 0;
     setXList(oList => [...oList, targetImage]);
+    setPrevImage(image);
     setCount(count => count + 1);
     setImage(imageList[count]);
-
+    translateX.value = withTiming(500, { duration: 300 });
+    rotate.value = withTiming(360, { duration: 500 });
     console.log('count 확인 ::::', count);
     console.log('xList 확인 ::::', xList);
     console.log('imageList 확인 ::::', imageList);
+    console.log('image 확인:::', image);
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      {/*<Animated.View*/}
-      {/*  style={{*/}
-      {/*    width: 100,*/}
-      {/*    height: 100,*/}
-      {/*    backgroundColor: 'violet',*/}
-      {/*  }}*/}
-      {/*/>*/}
-      <TouchableOpacity style={styles.button} onPress={() => handleClickO(image)}>
-        <Text style={styles.buttonText}>알아</Text>
-      </TouchableOpacity>
-      <View style={styles.itemBox}>
-        <View style={styles.question}>
-          <Image source={image} style={styles.image} resizeMode="contain" />
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <SafeAreaView style={styles.container}>
+        <TouchableOpacity style={styles.button} title="O" onPress={() => handleClickO(image)}>
+          <Text style={styles.buttonText}>O</Text>
+        </TouchableOpacity>
+        <View style={styles.itemBox}>
+          <View style={styles.imageContainer}>
+            <GestureDetector gesture={pan}>
+              <Animated.View style={[styles.circle, animatedStyle]}>
+                <Image source={image} style={styles.image} resizeMode="contain" />
+              </Animated.View>
+            </GestureDetector>
+          </View>
         </View>
-      </View>
-      <TouchableOpacity style={styles.button} onPress={() => handleClickX(image)}>
-        <Text style={styles.buttonText}>몰라</Text>
-      </TouchableOpacity>
-
-      {/*<View style={styles.buttonContainer}>*/}
-      {/*  <TouchableOpacity style={styles.button} title="O" onPress={() => handleClickO(image)}>*/}
-      {/*    <Text style={styles.buttonText}>O</Text>*/}
-      {/*  </TouchableOpacity>*/}
-      {/*  <TouchableOpacity*/}
-      {/*    style={styles.button}*/}
-      {/*    title="X"*/}
-      {/*    onPress={() => {*/}
-      {/*      handleClickX(image);*/}
-      {/*    }}*/}
-      {/*  >*/}
-      {/*    <Text style={styles.buttonText}>X</Text>*/}
-      {/*  </TouchableOpacity>*/}
-      {/*</View>*/}
-    </SafeAreaView>
+        <TouchableOpacity
+          style={styles.button}
+          title="X"
+          onPress={() => {
+            handleClickX(image);
+          }}
+        >
+          <Text style={styles.buttonText}>X</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
+  circle: {
+    height: 120,
+    width: 120,
+    borderRadius: 500,
+  },
   container: {
     backgroundColor: '#FFD1DC',
     justifyContent: 'center',
     alignItems: 'center',
+    display: 'flex',
     flex: 1,
   },
   itemBox: {
+    borderWidth: 1,
+    borderColor: 'white',
     display: 'flex',
     justifyContent: 'center',
     alignItems: 'center',
@@ -107,11 +156,20 @@ const styles = StyleSheet.create({
     elevation: 5,
     borderRadius: 10,
   },
-  question: {
-    width: 100,
-    height: 100,
-    // backgroundColor: 'black',
+  imageContainer: {
+    position: 'relative',
+
+    width: 150,
+    height: 150,
   },
+  image: {
+    position: 'absolute',
+    width: 150,
+    height: 150,
+    borderRadius: 20,
+    // borderWidth: 1,
+  },
+
   buttonContainer: {
     display: 'flex',
     flexDirection: 'row',
@@ -128,17 +186,11 @@ const styles = StyleSheet.create({
     backgroundColor: 'white',
     // borderWidth: 1,
     color: '#FFD1DC',
-    marginTop: 40,
   },
   buttonText: {
     fontSize: 40,
     fontWeight: '100',
     color: '#C8C8C8FF',
-  },
-  image: {
-    width: 100,
-    height: 100,
-    borderRadius: 10,
   },
 });
 
