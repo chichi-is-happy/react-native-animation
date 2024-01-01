1a0f66438872f072ca7e2056ceef44e81feab201
chichi <seonrye.yun@gmail.com>
2024-01-02 00:18:59 +0900

    chore: 각 버튼 CSS 수정 * absolute, left, top 기준으로 계산할 수 있도록 변경

--- src/pages/secondGame/SecondTest.js
+++ src/pages/secondGame/SecondTest.js
@@ -30,17 +30,34 @@ const SecondTest = () => {
   const imageX = useSharedValue(0);
   const imageY = useSharedValue(0);
   const scale = useSharedValue(0);
-  // const topButtonLayout = useSharedValue({ width: 0, height: 0, pageX: 0, pageY: 0 });
-  const bottomButtonLayout = useSharedValue({ width: 0, height: 0, pageX: 0, pageY: 0 });
   const upperButtonScale = useSharedValue(1);
   const lowerButtonScale = useSharedValue(1);
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const opacity = useSharedValue(1);
-  const [topButtonLayout, setTopButtonLayout] = useState({ pageX: 0, pageY: 0, width: 0, x: 0 });
+  const [topButtonLayout, setTopButtonLayout] = useState({
+    pageX: 0,
+    pageY: 0,
+    width: 0,
+    height: 0,
+  });
+  const [bottomButtonLayout, setBottomButtonLayout] = useState({
+    pageX: 0,
+    pageY: 0,
+    width: 0,
+    height: 0,
+  });
+
+  const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+
+  console.log('screenWidth:::', screenWidth);
+  console.log('screenHeight:::', screenHeight);
 
   useEffect(() => {
     console.log('topButtonLayout:::', topButtonLayout);
   }, [topButtonLayout]);
+  useEffect(() => {
+    console.log('bottomButtonLayout:::', bottomButtonLayout);
+  }, [bottomButtonLayout]);
 
   const increaseButtonSize = buttonScale => {
     buttonScale.value = 1.5;
@@ -51,32 +68,22 @@ const SecondTest = () => {
     buttonScale.value = 1; // 원래 크기로 복구
   };
 
-  const onButtonLayout = (event, buttonLayout) => {
-    const layout = event.nativeEvent.layout;
-    buttonLayout.value = layout;
-  };
-
   const imageStyle = useAnimatedStyle(() => ({
     transform: [{ scale: scale.value }, { translateX: imageX.value }, { translateY: imageY.value }],
     alignItems: 'center',
     justifyContent: 'center',
     opacity: opacity.value,
+    zIndex: 999,
   }));
 
   const topButtonStyle = useAnimatedStyle(() => ({
-    transform: [
-      { translateX: topButtonLayout.pageX && topButtonLayout.pageX },
-      { translateY: topButtonLayout.pageY && topButtonLayout.pageY },
-      { scale: upperButtonScale.value },
-    ],
+    transform: [{ scale: upperButtonScale.value }],
+    zIndex: -999,
   }));
 
   const bottomButtonStyle = useAnimatedStyle(() => ({
-    transform: [
-      { translateX: bottomButtonLayout.value.pageX },
-      { translateY: bottomButtonLayout.value.pageY },
-      { scale: lowerButtonScale.value },
-    ],
+    transform: [{ scale: lowerButtonScale.value }],
+    zIndex: -999,
   }));
 
   // 이미지 크기가 증가하는 애니메이션
@@ -91,25 +98,44 @@ const SecondTest = () => {
     .onChange(event => {
       imageX.value = event.translationX;
       imageY.value = event.translationY;
+      console.log('event.translationX :::', event.translationX);
+      console.log('topButtonLayout.pageX :::', topButtonLayout.pageX);
+
+      const imageCenterX = imageX.value + screenWidth / 2;
+      const imageCenterY = imageY.value + screenHeight / 2;
+
+      console.log('imageCenterX:::', imageCenterX);
+      console.log('imageCenterY:::', imageCenterY);
 
       if (
-        // event.translationX >= topButtonLayout.value.pageX &&
-        // event.translationX <= topButtonLayout.value.pageX + topButtonLayout.value.width &&
-        // event.translationY >= topButtonLayout.value.y &&
-        // event.translationY <= topButtonLayout.value.y + topButtonLayout.value.height
-        event.translationX
-        //
+        imageCenterX >= topButtonLayout.pageX &&
+        imageCenterX <= topButtonLayout.pageX + topButtonLayout.width &&
+        imageCenterY >= topButtonLayout.pageY &&
+        imageCenterY <= topButtonLayout.pageY + topButtonLayout.height
       ) {
         runOnJS(increaseButtonSize)(upperButtonScale);
-        upperButtonScale.value = 2;
-        console.log('버튼 범위 안');
+        upperButtonScale.value = withSpring(2, { duration: 300 });
+        console.log('O 버튼 범위 안');
+      } else if (
+        imageCenterX >= bottomButtonLayout.pageX &&
+        imageCenterX <= bottomButtonLayout.pageX + bottomButtonLayout.width &&
+        imageCenterY >= bottomButtonLayout.pageY &&
+        imageCenterY <= bottomButtonLayout.pageY + bottomButtonLayout.height
+      ) {
+        runOnJS(increaseButtonSize)(lowerButtonScale);
+        lowerButtonScale.value = withSpring(2, { duration: 300 });
+        console.log('X 버튼 범위 안');
       } else {
-        upperButtonScale.value = 1;
-        runOnJS(resetButtonSize)(upperButtonScale);
+        // upperButtonScale.value = 1;
+        upperButtonScale.value = withSpring(1, { duration: 300 });
+        lowerButtonScale.value = withSpring(1, { duration: 300 });
+        // runOnJS(resetButtonSize)(upperButtonScale);
       }
     })
     .onFinalize(() => {
       console.log('종료');
+      upperButtonScale.value = withSpring(1, { duration: 300 });
+      lowerButtonScale.value = withSpring(1, { duration: 300 });
       runOnJS(updateCurrentIndexAndScale)();
     });
 
@@ -139,109 +165,109 @@ const SecondTest = () => {
   const secondButtonRef = useRef(null);
 
   return (
-    <Animated.View
+    <View
       style={{
         flex: 1,
-        // height: ß2200,ß
         backgroundColor: '#FFF',
         alignItems: 'center',
         justifyContent: 'center',
         flexDirection: 'column',
         borderWidth: 1,
-        //   justifyContent: 'space-between',
-        //   alignItems: 'center',
-        //   flexDirection: 'column',
       }}
     >
       {/*<StatusBar hidden={true} />*/}
-      <View style={styles.buttonContainer}>
-        <Animated.View
-          ref={firstButtonRef}
-          onLayout={() => {
-            if (firstButtonRef) {
-              // firstButtonRef.current.measure((x, y, width, height, pageX, pageY) => {
-              //   console.log(x, y, width, height, pageX, pageY);
-              // });
-              firstButtonRef.current.measure((x, y, width, height, pageX, pageY) => {
-                console.log(x, y, width, height, pageX, pageY);
-                setTopButtonLayout({ pageX: pageX, pageY: pageY, width: width, height: height });
-              });
-            }
-          }}
-          style={[
-            {
-              // width: 100,
-              // height: 100,
-              borderWidth: 1,
-              color: '#FFD1DC',
-              fontSize: 40,
-              fontWeight: '100',
-            },
-            topButtonStyle,
-          ]}
-        >
-          <TouchableOpacity title="O" style={styles.button}>
-            <Text style={styles.buttonText}>O</Text>
-          </TouchableOpacity>
-        </Animated.View>
+      {/*<View style={styles.buttonContainer}>*/}
+      <Animated.View
+        ref={firstButtonRef}
+        onLayout={() => {
+          if (firstButtonRef) {
+            firstButtonRef.current.measure((x, y, width, height) => {
+              console.log('위', width, height, x, y);
+              setTopButtonLayout({ pageX: x, pageY: y, width: width, height: height });
+            });
+          }
+        }}
+        style={[
+          {
+            position: 'absolute',
+            left: 160,
+            top: 70,
+            borderWidth: 1,
+            color: '#FFD1DC',
+            fontSize: 40,
+            fontWeight: '100',
+          },
+          topButtonStyle,
+        ]}
+      >
+        <TouchableOpacity title="O" style={styles.button}>
+          <Text style={styles.buttonText}>O</Text>
+        </TouchableOpacity>
+      </Animated.View>
 
-        <GestureHandlerRootView>
-          <GestureDetector gesture={pan}>
-            <Animated.View style={[imageStyle]}>
-              <View style={styles.imageContainer}>
-                <Image source={images[currentImageIndex]} style={styles.image} />
-              </View>
-            </Animated.View>
-          </GestureDetector>
-        </GestureHandlerRootView>
+      <GestureHandlerRootView>
+        <GestureDetector gesture={pan}>
+          <Animated.View style={[imageStyle]}>
+            <View style={styles.imageContainer}>
+              <Image source={images[currentImageIndex]} style={styles.image} />
+            </View>
+          </Animated.View>
+        </GestureDetector>
+      </GestureHandlerRootView>
 
-        <Animated.View
-          style={[
-            {
-              // width: 100,
-              // height: 100,
-              borderWidth: 1,
-              color: '#FFD1DC',
-              fontSize: 40,
-              fontWeight: '100',
-            },
-            bottomButtonStyle,
-          ]}
-          ref={secondButtonRef}
-          onLayout={() => {
-            if (secondButtonRef) {
-              secondButtonRef.current.measure((x, y, width, height, pageX, pageY) => {
-                console.log(x, y, width, height, pageX, pageY);
-              });
-            }
-          }}
-        >
-          <TouchableOpacity title="X" style={styles.button}>
-            <Text style={styles.buttonText}>X</Text>
-          </TouchableOpacity>
-        </Animated.View>
-      </View>
-    </Animated.View>
+      <Animated.View
+        ref={secondButtonRef}
+        onLayout={() => {
+          if (secondButtonRef) {
+            secondButtonRef.current.measure((x, y, width, height) => {
+              console.log('아래', width, height, x, y);
+              setBottomButtonLayout({ pageX: x, pageY: y, width: width, height: height });
+            });
+          }
+        }}
+        style={[
+          {
+            position: 'absolute',
+            left: 160,
+            top: 500,
+            borderWidth: 1,
+            color: '#FFD1DC',
+            fontSize: 40,
+            fontWeight: '100',
+          },
+          bottomButtonStyle,
+        ]}
+      >
+        <TouchableOpacity title="X" style={styles.button}>
+          <Text style={styles.buttonText}>X</Text>
+        </TouchableOpacity>
+      </Animated.View>
+      {/*</View>*/}
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   imageContainer: {
     borderWidth: 1,
+    borderColor: 'pink',
+    zIndex: 999,
   },
   image: {
     width: 20,
     height: 20,
     borderWidth: 1,
     borderRadius: 20,
+    zIndex: 999,
   },
   buttonContainer: {
     // width: '100%',
     height: '80%',
-    justifyContent: 'space-between',
+    // justifyContent: 'space-between',
     alignItems: 'center',
     flexDirection: 'column',
     borderWidth: 1,
+    borderColor: 'red',
   },
   button: {
     alignItems: 'center',
